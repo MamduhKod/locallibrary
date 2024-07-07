a2786b4b468599b25c913ac4fdc21339276a0380
Mamduh Halawa <mamduhkod@gmail.com>
2024-07-07 23:12:15 +0200

    more stuff

--- catalog/admin.py
+++ catalog/admin.py
@@ -13,6 +13,7 @@ admin.site.register(Genre)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
@@ -24,4 +25,5 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
     pass
